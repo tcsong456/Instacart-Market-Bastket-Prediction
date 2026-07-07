3fa9959ac5b1c9eb6ec4322ae44fa802297f3ed9
Andy Song <congxisong@hotmail.com>
2026-07-07 21:42:33 +0100

    Try raw_dir in place of fake_dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -184,7 +184,7 @@ def fake_filtered_orders(spark, raw_dir):
 
 
 @pytest.fixture
-def fake_orders(fake_dir):
+def fake_orders(raw_dir):
     orders = pd.DataFrame(
         [
             (10, 1, "train", 3, 23),
@@ -194,13 +194,13 @@ def fake_orders(fake_dir):
         ],
         columns=["user_id", "order_id", "eval_set", "order_dow", "order_hour_of_day"],
     )
-    order_path = fake_dir / "orders.csv"
+    order_path = raw_dir / "orders.csv"
     orders.to_csv(order_path, index=False)
-    return fake_dir
+    return raw_dir
 
 
 @pytest.fixture
-def fake_products_data(fake_dir):
+def fake_products_data(raw_dir):
     products = pd.DataFrame(
         [
             (1, "b", 5, 10),
@@ -220,9 +220,9 @@ def fake_products_data(fake_dir):
         ],
         columns=["product_id", "product_name", "aisle_id", "department_id"],
     )
-    product_path = fake_dir / "products.csv"
+    product_path = raw_dir / "products.csv"
     products.to_csv(product_path, index=False)
-    return fake_dir
+    return raw_dir
 
 
 @pytest.fixture
