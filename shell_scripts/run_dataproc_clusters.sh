75bbb69ea48065f1132e86c548362f9166ffa3a2
Andy Song <congxisong@hotmail.com>
2026-07-10 11:16:14 +0100

    Modified the run_dataproc_cluster.sh to run the whole etl pipeline

--- shell_scripts/run_dataproc_clusters.sh
+++ shell_scripts/run_dataproc_clusters.sh
@@ -1,9 +1,13 @@
-gcloud storage rsync -r data/raw gs://instacart-raw-8f061ed7/raw
-gcloud storage ls gs://instacart-raw-8f061ed7/raw
+# gcloud storage rsync -r data/raw gs://instacart-raw-8f061ed7/raw
+# gcloud storage ls gs://instacart-raw-8f061ed7/raw
 
-python -m pytest \
-    tests/unit_test/test_create_sample_dataset.py \
-    tests/integration_test/test_create_sample_dataset_pipeline.py
+file=$1
+if [[ "$file" == "curated" ]]; then
+    bronze_input_level="raw"
+else
+    bronze_input_level="sample"
+fi
+
 python -m src.ingestion.create_sample_dataset \
     --raw-dir data/raw \
     --sample-dir data/sample \
@@ -14,7 +18,6 @@ python -m src.ingestion.create_sample_dataset \
 gcloud storage rsync -r data/sample gs://instacart-raw-8f061ed7/sample
 gcloud storage ls gs://instacart-raw-8f061ed7/sample
 
-python -m pytest tests/integration_test/test_create_bronze_datasets_pipeline.py
 python -m zipfile -c src.zip src
 gcloud dataproc jobs submit pyspark \
     src/ingestion/create_bronze_datasets.py \
@@ -22,8 +25,8 @@ gcloud dataproc jobs submit pyspark \
     --region=europe-west1 \
     --py-files=src.zip \
     -- \
-    --input-dir gs://instacart-raw-8f061ed7/raw \
-    --output-dir gs://instacart-bronze-8f061ed7/raw
+    --input-dir gs://instacart-raw-8f061ed7/$bronze_input_level \
+    --output-dir gs://instacart-bronze-8f061ed7/$file
 
 gcloud dataproc jobs submit pyspark \
     src/ingestion/create_order_products.py \
@@ -31,8 +34,8 @@ gcloud dataproc jobs submit pyspark \
     --region=europe-west1 \
     --py-files=src.zip \
     -- \
-    --input-dir=gs://instacart-bronze-8f061ed7/raw \
-    --output-dir=gs://instacart-silver-8f061ed7/raw
+    --input-dir=gs://instacart-bronze-8f061ed7/$file \
+    --output-dir=gs://instacart-silver-8f061ed7/$file
 
 gcloud dataproc jobs submit pyspark \
     src/ingestion/create_user_data.py \
@@ -40,8 +43,8 @@ gcloud dataproc jobs submit pyspark \
     --region=europe-west1 \
     --py-files=src.zip \
     -- \
-    --input-dir=gs://instacart-silver-8f061ed7/raw/order_products \
-    --output-dir=gs://instacart-silver-8f061ed7/raw/user_data
+    --input-dir=gs://instacart-silver-8f061ed7/$file/order_products \
+    --output-dir=gs://instacart-silver-8f061ed7/$file/user_data
 
 gcloud dataproc jobs submit pyspark \
     src/ingestion/create_product_history_data.py \
@@ -49,6 +52,6 @@ gcloud dataproc jobs submit pyspark \
     --region=europe-west1 \
     --py-files=src.zip \
     -- \
-    --input-dir=gs://instacart-silver-8f061ed7/raw \
-    --raw-dir=gs://instacart-bronze-8f061ed7/raw \
-    --output-dir=gs://instacart-silver-8f061ed7/raw/product_history_data
+    --input-dir=gs://instacart-silver-8f061ed7/$file \
+    --raw-dir=gs://instacart-bronze-8f061ed7/$file \
+    --output-dir=gs://instacart-silver-8f061ed7/$file/product_history_data
