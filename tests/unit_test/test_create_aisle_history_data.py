178f87adaf4f63b9447e90e787b0407a5caab236
Andy Song <congxisong@hotmail.com>
2026-07-11 00:20:48 +0100

    Added expected schma inside test_parse_seq

--- tests/unit_test/test_create_aisle_history_data.py
+++ tests/unit_test/test_create_aisle_history_data.py
@@ -2,12 +2,52 @@ from pyspark.sql import Row
 from src.common.io import read_parquet
 from tests.helper import assert_spark_df_equal
 from src.ingestion.create_aisle_history_data import parse_seq
+from pyspark.sql.types import (
+    StringType,
+    IntegerType,
+    LongType,
+    StructField,
+    StructType,
+    ArrayType,
+)
 
 
 def test_parse_seq(fake_user_data, spark):
+    COLUMNS = [
+        "user_id",
+        "aislle_raw",
+        "aisle_prev",
+        "aisle_next",
+        "aisle_all",
+        "aisle_set",
+        "next_aisle_set",
+    ]
+
     user_data = read_parquet(fake_user_data / "user_data", spark)
     actual_df = parse_seq(user_data)
+    actual_df = actual_df.select(COLUMNS)
 
+    expected_schema = StructType(
+        [
+            StructField("user_id", LongType(), True),
+            StructField("aisle_raw", ArrayType(StringType(), containsNull=False), True),
+            StructField(
+                "aisle_prev", ArrayType(StringType(), containsNull=False), True
+            ),
+            StructField("aisle_next", StringType(), True),
+            StructField(
+                "aisle_all",
+                ArrayType(
+                    ArrayType(IntegerType(), containsNull=True), containsNull=False
+                ),
+                True,
+            ),
+            StructField("aisle_set", ArrayType(IntegerType(), containsNull=True), True),
+            StructField(
+                "next_aisle_set", ArrayType(IntegerType(), containsNull=True), True
+            ),
+        ]
+    )
     expected_df = spark.createDataFrame(
         [
             Row(
@@ -28,9 +68,10 @@ def test_parse_seq(fake_user_data, spark):
                 aisle_set=[5, 15, 20],
                 next_aisle_set=[5, 20, 25],
             ),
-        ]
+        ],
+        schema=expected_schema,
     )
-    actual_df.printSchema()
+
     assert_spark_df_equal(
         actual_df, expected_df, ["user_id"], ["aisle_set", "next_aisle_set"]
     )
