1866092c2ad6d1e5943940e34ee065ade2821fd0
Andy Song <congxisong@hotmail.com>
2026-07-11 13:02:28 +0100

    Added integration test for aisle_history_ata

---
+++ tests/integration_test/test_aisle_history_data_pipeline.py
@@ -0,0 +1,105 @@
+from pyspark.sql import Row
+from src.common.io import read_parquet
+from src.common.utils import gcs_join
+from tests.helper import assert_spark_df_equal
+from src.ingestion.create_aisle_history_data import build_create_aisle_history_data
+
+
+def test_aisle_history_data_pipeline(
+    spark, fake_user_data, fake_products_data, tmp_path
+):
+    output_dir = tmp_path / "curated"
+    output_dir.mkdir()
+    build_create_aisle_history_data(
+        spark=spark,
+        input_dir=fake_user_data,
+        raw_dir=fake_products_data,
+        output_dir=str(output_dir),
+    )
+    actual_df = read_parquet(gcs_join(output_dir, "aisle_history_data"), spark)
+
+    common_cols_10 = dict(
+        aisle_history_size="3 2 3",
+        order_dows="5 0 5 1",
+        order_hours="23 1 17 16",
+        days_since_prior_orders="-1 30 60 25",
+        order_numbers="1 2 3 4",
+        eval_set="train",
+    )
+    common_cols_20 = dict(
+        aisle_history_size="1 1 3",
+        order_dows="1 2 3 4",
+        order_hours="8 13 8 0",
+        days_since_prior_orders="10 19 22 6",
+        order_numbers="3 4 5 6",
+        eval_set="test",
+    )
+    expected_df = spark.createDataFrame(
+        [
+            Row(
+                user_id=10,
+                aisle_id=5,
+                department_id=10,
+                is_ordered_history="1 0 1",
+                position_in_order="1 0 1",
+                num_products_from_aisle="1 0 2",
+                **common_cols_10,
+            ),
+            Row(
+                user_id=10,
+                aisle_id=10,
+                department_id=20,
+                is_ordered_history="1 1 1",
+                position_in_order="2 1 2",
+                num_products_from_aisle="1 1 1",
+                **common_cols_10,
+            ),
+            Row(
+                user_id=10,
+                aisle_id=15,
+                department_id=30,
+                is_ordered_history="1 0 1",
+                position_in_order="3 0 3",
+                num_products_from_aisle="1 0 1",
+                **common_cols_10,
+            ),
+            Row(
+                user_id=10,
+                aisle_id=20,
+                department_id=40,
+                is_ordered_history="0 1 0",
+                position_in_order="0 2 0",
+                num_products_from_aisle="0 1 0",
+                **common_cols_10,
+            ),
+            Row(
+                user_id=20,
+                aisle_id=5,
+                department_id=10,
+                is_ordered_history="1 0 1",
+                position_in_order="1 0 3",
+                num_products_from_aisle="2 0 1",
+                **common_cols_20,
+            ),
+            Row(
+                user_id=20,
+                aisle_id=15,
+                department_id=30,
+                is_ordered_history="0 1 1",
+                position_in_order="0 1 1",
+                num_products_from_aisle="0 1 2",
+                **common_cols_20,
+            ),
+            Row(
+                user_id=20,
+                aisle_id=20,
+                department_id=40,
+                is_ordered_history="0 0 1",
+                position_in_order="0 0 2",
+                num_products_from_aisle="0 0 2",
+                **common_cols_20,
+            ),
+        ]
+    )
+    actual_df.printSchema()
+    assert_spark_df_equal(actual_df, expected_df, ["user_id", "aisle_id"])
