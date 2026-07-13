9a77f91fc15b0d2ce24a9886bba3b6d0643e0b83
Andy Song <congxisong@hotmail.com>
2026-07-13 21:31:02 +0100

    Completed function pad_column_arrays

--- tests/unit_test/test_reorder_size_training_data.py
+++ tests/unit_test/test_reorder_size_training_data.py
@@ -2,6 +2,7 @@ from pyspark.sql import Row
 from tests.helper import assert_spark_df_equal
 from src.ingestion.prepare_reorder_size_training_data import (
     transform_reorder_size_training_data,
+    pad_column_arrays,
 )
 from pyspark.sql.types import (
     ArrayType,
@@ -36,11 +37,7 @@ def test_transform_reorder_size_training_data(spark):
 
     expected_schema = StructType(
         [
-            StructField(
-                "user_id",
-                LongType(),
-                nullable=True,
-            ),
+            StructField("user_id", LongType(), nullable=True),
             StructField(
                 "reorders_prev",
                 ArrayType(
@@ -54,33 +51,20 @@ def test_transform_reorder_size_training_data(spark):
             ),
             StructField(
                 "reorders_next",
-                ArrayType(
-                    IntegerType(),
-                    containsNull=True,
-                ),
+                ArrayType(IntegerType(), containsNull=True),
                 nullable=True,
             ),
             StructField(
                 "order_sizes",
-                ArrayType(
-                    IntegerType(),
-                    containsNull=False,
-                ),
+                ArrayType(IntegerType(), containsNull=False),
                 nullable=True,
             ),
             StructField(
                 "reorder_sizes",
-                ArrayType(
-                    IntegerType(),
-                    containsNull=True,
-                ),
+                ArrayType(IntegerType(), containsNull=True),
                 nullable=True,
             ),
-            StructField(
-                "label",
-                IntegerType(),
-                nullable=True,
-            ),
+            StructField("label", IntegerType(), nullable=True),
         ]
     )
     expected_df = spark.createDataFrame(
@@ -137,4 +121,87 @@ def test_transform_reorder_size_training_data(spark):
         schema=expected_schema,
     )
 
+    assert_spark_df_equal(actual_df, expected_df, ["user_id"])
+
+
+def test_pad_column_arrays(spark):
+    df = spark.createDataFrame(
+        [
+            Row(
+                user_id=1,
+                order_numbers="1 2 3 4 5",
+                order_dows="",
+                order_hours="10 20 5 0 9",
+                days_since_prior_orders="-1 10 5 20 30",
+                order_sizes=[5, 10, 21, 13],
+                reorder_sizes=[3, 4, 9, 5],
+            ),
+            Row(
+                user_id=2,
+                order_numbers="1 2 3 4 5 6 7",
+                order_dows="6 6 3 2 2 5 0",
+                order_hours=None,
+                days_since_prior_orders="-1 30 31 20 7 11 18",
+                order_sizes=[4, 3, 7, 10, 1, 9],
+                reorder_sizes=[0, 3, 1, 5, 1, 6],
+            ),
+            Row(
+                user_id=3,
+                order_numbers="1 2 3 4 5 6 7 8 9",
+                order_dows="5 1 2 3 0 2 1 6 4",
+                order_hours="23 1 17 14 20 20 8 19 12",
+                days_since_prior_orders="-1 14 15 22 13 28 10 16 25",
+                order_sizes=[20, 23, 24, 23, 14, 9, 11, 8],
+                reorder_sizes=[],
+            ),
+        ]
+    )
+
+    actual_df = pad_column_arrays(df, 7)
+    actual_df = actual_df.select(
+        "user_id",
+        "order_dows",
+        "order_hours",
+        "days_since_prior_orders",
+        "order_numbers",
+        "order_sizes",
+        "reorder_sizes",
+        "history_length",
+    )
+
+    expected_df = spark.createDataFrame(
+        [
+            Row(
+                user_id=1,
+                order_numbers=[1, 2, 3, 4, 5, 0, 0],
+                order_dows=[0, 0, 0, 0, 0, 0, 0],
+                order_hours=[10, 20, 5, 0, 9, 0, 0],
+                days_since_prior_orders=[-1, 10, 5, 20, 30, 0, 0],
+                order_sizes=[5, 10, 21, 13, 0, 0, 0],
+                reorder_sizes=[3, 4, 9, 5, 0, 0, 0],
+                history_length=4,
+            ),
+            Row(
+                user_id=2,
+                order_numbers=[1, 2, 3, 4, 5, 6, 7],
+                order_dows=[6, 6, 3, 2, 2, 5, 0],
+                order_hours=[0, 0, 0, 0, 0, 0, 0],
+                days_since_prior_orders=[-1, 30, 31, 20, 7, 11, 18],
+                order_sizes=[4, 3, 7, 10, 1, 9, 0],
+                reorder_sizes=[0, 3, 1, 5, 1, 6, 0],
+                history_length=6,
+            ),
+            Row(
+                user_id=3,
+                order_numbers=[1, 2, 3, 4, 5, 6, 7],
+                order_dows=[5, 1, 2, 3, 0, 2, 1],
+                order_hours=[23, 1, 17, 14, 20, 20, 8],
+                days_since_prior_orders=[-1, 14, 15, 22, 13, 28, 10],
+                order_sizes=[20, 23, 24, 23, 14, 9, 11],
+                reorder_sizes=[0, 0, 0, 0, 0, 0, 0],
+                history_length=0,
+            ),
+        ]
+    )
+    actual_df.printSchema()
     assert_spark_df_equal(actual_df, expected_df, ["user_id"])
